a9f4dadfe7811a815f94fa9f8a22de8c4668e05e
Carl-Erik Kopseng <carlerik@gmail.com>
2018-02-28 21:11:50 +0100

    Add tests for the Module object

--- mod1spec.js
+++ mod1spec.js
@@ -1,14 +1,28 @@
 // mod1Spec.js
-import chai from 'chai';
-import sinon from 'sinon';
+import chai from "chai";
+import sinon from "sinon";
 
-import * as mod1 from './mod1';
+import * as mod1 from "./mod1";
 
 const assert = chai.assert;
 
-describe('test', function () {
-  it('should correctly mock module method', () => {
-    sinon.stub(mod1, 'test').returns('pass');
-    assert.strictEqual(mod1.test(), 'pass');
+describe("test", function() {
+  it("should correctly mock module method", () => {
+    sinon.stub(mod1, "test").returns("pass");
+    assert.strictEqual(mod1.test(), "pass");
+  });
+});
+
+describe("the Module object", function() {
+  it("should have a toStringTag symbol with the expected value", () => {
+    assert.strictEqual(mod1[Symbol.toStringTag], "Module");
+  });
+
+  it("should use the toStringTag symbol", () => {
+    const symbolString = mod1[Symbol.toStringTag];
+    assert.strictEqual(
+      Object.prototype.toString.call(mod1),
+      `[object ${symbolString}]`
+    );
   });
 });
